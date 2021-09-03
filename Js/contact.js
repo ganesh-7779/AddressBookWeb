8103dbd87a86365edaae9b82ec8ae9f022782bec
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-09-03 21:26:18 +0530

    Added : UC4 for validation user input

---
+++ Js/contact.js
@@ -0,0 +1,57 @@
+/*
+Purpose :- Pojo class of all attribute use in Address book wage page.
+@author :- Ganesh Gavhad
+@Version :- 1.0
+@Since :- 03/09/2021
+*/
+class AddressBook{
+
+    get id(){return this._id}
+    set id(id){
+        this._id=id;
+    }
+
+    get name(){ return this._name;}
+    set name(name) {
+         let nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+         if(nameRegex.test(name))
+           this._name=name;
+        else throw 'Name is Incorrect';
+    }
+    get address(){return this._address}
+    set address(address){
+        this._address=address;
+    }
+
+    get city(){return this._city}
+    set city(city){
+        this._city=city;
+    }
+
+    get state(){return this._state}
+    set state(state){
+        this._state=state;
+    }
+
+    get zipCode(){return this._zipCode}
+    set zipCode(zipCode){
+        let zipRegex=RegExp('^[0-9]{6}$');
+        if(zipRegex.test(zipCode))
+          this._zipCode=zipCode;
+       else throw 'ZipCode is Incorrect';
+    }
+
+    get phoneNo(){return this._phoneNo}
+    set phoneNo(phoneNo){
+        let mobileRegex=RegExp('^[0-9]{10}$');
+        if(mobileRegex.test(phoneNo))
+          this._phoneNo=phoneNo;
+       else throw 'Number is Incorrect';
+    }
+
+    toString(){
+        return "id = "+this.id + " name = "+this.name+" address = "+this.address+ 
+               " city = "+this.city+ " state = "+this.state +" zipCode = "+
+               this.zipCode + " phoneNo = "+this.phoneNo;
+    }
+}
